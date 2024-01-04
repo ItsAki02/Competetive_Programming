136a812f36e4cf304f894e812877b9fad3ce25d7
Akanksha <98252390+ItsAki02@users.noreply.github.com>
2024-01-04 10:28:50 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find element occuring once when all other are present thrice/find-element-occuring-once-when-all-other-are-present-thrice.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for JAVA
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            
+            int[] arr = new int[N];
+            
+            for(int i=0 ; i<N ; i++)
+                arr[i] = Integer.parseInt(S[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.singleElement(arr,N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int singleElement(int[] arr , int N) {
+        // code here
+    Map<Integer,Integer> map = new HashMap<>();
+    
+    for(int i = 0; i< N; i++){
+        if(!map.containsKey(arr[i]))
+                map.put(arr[i],0);
+              
+            map.put(arr[i],map.get(arr[i])+1);
+    }
+    
+    for(Map.Entry<Integer,Integer> e : map.entrySet()){
+        if(e.getValue() == 1){
+            return e.getKey();
+        }
+    }
+        return -1;
+    }
+}
